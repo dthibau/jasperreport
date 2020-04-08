713a4bb7aa235813e23d59496171438100733a55
David THIBAU <david.thibau@gmail.com>
2020-04-08 10:03:25 +0200

    Sample Eclipse Project avec intégration scriptlet

---
+++ coop/src/com/cityzen/jasper/Test.java
@@ -0,0 +1,12 @@
+package com.cityzen.jasper;
+
+import net.sf.jasperreports.engine.JRDefaultScriptlet;
+
+public class Test extends JRDefaultScriptlet {
+
+	
+	public String hello() {
+		return "Hello world !";
+	}
+
+}
